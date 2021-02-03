03b9ec3ba09c03e94e97c37c8cd9eabe1dfa6fc3
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-02-03 14:59:31 +0530

    add gitignore and optimised otpgenerator

--- otpgenerator.py
+++ otpgenerator.py
@@ -4,9 +4,12 @@ import string
 capletter = string.ascii_uppercase
 letters = string.ascii_lowercase
 numbers = string.digits
+mix = letters + numbers
 
 for i in range(10):
-    otp = (random.choice(capletter), random.choice(numbers),
-           random.choice(letters), random.choice(numbers), random.choice(letters))
-    out = "".join(otp)
+    # otp = (random.choice(capletter), random.choice(numbers),
+    #        random.choice(letters), random.choice(numbers), random.choice(letters))
+    # out = "".join(otp)
+    otp = "".join(random.choices(mix, k=4))
+    out = random.choice(capletter) + otp
     print(out)
